5fdd63b1c6690716711856b5eb36f01ce7fe10c4
Ryu <richarstorm@gmail.com>
2026-04-02 20:16:03 -0400

    Add BCrypt and UsersController

---
+++ RealEstateAPI/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<UsersController> _logger;
+
+
+        public UsersController(ApplicationDbContext context, ILogger<UsersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+
+    }
+}
